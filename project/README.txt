185190e746f86deef7727fad58e42bc5b4bdf6aa
joannadubi <ja.dubiel@gmail.com>
2017-02-18 18:39:24 +0100

    Adding files to repository

---
+++ project/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/Joanna_D/pen/RKpQJr.
+
+ 
---
+++ project/js/index.js
@@ -0,0 +1,20 @@
+// your JS goes here if needed
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+move();
+
+function showDiv() {
+   document.getElementById('pop-up').style.display = "block";
+}
